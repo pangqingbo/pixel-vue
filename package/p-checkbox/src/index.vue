18c9d0cf8f94e10f2ce3c3ef73d54d9cb02db37f
pangqingbo <2362698454@qq.com>
2021-10-28 16:02:04 +0800

    update checkbox component

--- package/p-checkbox/src/index.vue
+++ package/p-checkbox/src/index.vue
@@ -1,12 +1,10 @@
 <template>
-    <div id="pcheckContainer" :class="{ checked }" @click="toggleChecked">
-        <i class="fas fa-check"></i>
+    <div id="pcheckContainer" :class="{ checked }">
+        
     </div>
 </template>
 
 <script>
-// import { ref } from '@vue/reactivity'
-import "../../public/icon/css/all.min.css"
 export default {
     name: 'PCheckbox',
     props: {
@@ -18,41 +16,10 @@ export default {
             type: Boolean,
             default: false
         },
-    },
-    setup(){
-        function toggleChecked(e){
-            console.log(e.target.className.split(' ').filter(item => {
-                item !== 'checked'
-            }))
-        }
-        return{
-            toggleChecked
-        }
     }
 }
 </script>
 
 <style scoped>
-#pcheckContainer{
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-    background: #ebfafc;
-    box-shadow: -1px -1px 3px rgba(255, 255, 255, 1),
-                3px 3px 5px rgba(0, 0, 0, .1);
-}
-#pcheckContainer i{
-    font-size: 10px;
-    pointer-events: none;
-}
-#pcheckContainer.checked{
-    transform: scale(0.95);
-    background: linear-gradient(yellow, red);
-    color: #fff;
-    box-shadow: inset -1px -1px 3px rgba(255, 255, 255, 1),
-                inset 3px 3px 5px rgba(0, 0, 0, .1);
-}
+
 </style>
